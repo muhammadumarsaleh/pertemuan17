d371d173e421731c54f01bf88a88b4be81288650
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2022-01-05 14:22:24 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# pertemuan17
- Menambahkan fitur Remember Me pada Login System menggunakan konsep COOKIE
+# pertemuan19
+ 
--- index.php
+++ index.php
@@ -31,11 +31,11 @@ if( isset($_POST["cari"]) ) {
 <a href="tambah.php">Tambah data mahasiswa</a>
 <br><br>
 <form action="" method="post">
-    <input type="text" name ="keyword" size="30" autofocus placeholder="Masukkan keyword pencarian.." autocomplete="off">
-    <button type="submit" name="cari">Cari!</button>
+    <input type="text" name ="keyword" size="30" autofocus placeholder="Masukkan keyword pencarian.." autocomplete="off" id="keyword">
+    <button type="submit" name="cari" id="tombol-cari">Cari!</button>
 </form>
 <br>
-
+<div id='container'>
 <table border="1" cellpadding="10" cellspacing="0">
     <tr>
         <th>No.</th>
@@ -65,5 +65,8 @@ if( isset($_POST["cari"]) ) {
     <?php endforeach; ?>
 
 </table>
+</div>
+
+<script src="js/script.js" ></script>
 </body>
 </html>
